9a0275df872e93fcbac94dd3a82c7405bae2859f
Mariem Ben Tamansourt <bentamansourt.mariem@gmail.com>
2020-04-17 23:51:03 +0100

    first commit

---
+++ signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="stylesign.css">
+<title>Sign up - E-learning for kids</title>
+</head>
+<body>
+<img src="logo.png" alt="logo"/>
+<h1> E-learning for kids<h1>
+<p>Create an account</p>
+<a href="facebook.com"><figure><img src="Facebooklogo.png" alt="facebook"/><figcaption>Sign up with facebook</figcaption><figure></a>
+<p>-or-</p>
+<form method="post" action="completeyourprofile.HTML">
+<p>
+<label for="email"> Your e-mail: </label>
+<input type="email" name="email" id="email"/>
+<br />
+<label for="password"> Your password: </label>
+<input type="password" name="password" id="password"/>
+<br />
+</p>
+</form>
+<input type="submit" name="Get started"/>
+<p>Already have account? <a href="signin.HTML">sign in</a>
+<h2>Learn and play</h2>
+<p>tex tt ext text textt ext textte xt te xtte xtte xttex ttextte xttextte xtt extt exttextt exttext texttext textt extt ext texttext text</p>
+</body>
+</html>
